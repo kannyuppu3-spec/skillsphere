a5d46beb4b2cb3c420e147a3624d48caefdc4b6c
kannyuppu <kannyuppu3@gmail.com>
2026-07-13 13:48:06 +0530

    Day 7 completed - Freelancer Profile CRUD

--- backend/controllers/FreelancerController.js
+++ backend/controllers/FreelancerController.js
@@ -32,6 +32,84 @@ const createProfile = async (req, res) => {
   }
 };
 
+const getProfile = async (req, res) => {
+  try {
+    const profile = await FreelancerProfile.findOne({
+      user: req.user.id,
+    });
+
+    if (!profile) {
+      return res.status(404).json({
+        message: "Profile not found",
+      });
+    }
+
+    res.json(profile);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+const updateProfile = async (req, res) => {
+  try {
+    const profile = await FreelancerProfile.findOne({
+      user: req.user.id,
+    });
+
+    if (!profile) {
+      return res.status(404).json({
+        message: "Profile not found",
+      });
+    }
+
+    profile.title = req.body.title || profile.title;
+    profile.skills = req.body.skills || profile.skills;
+    profile.bio = req.body.bio || profile.bio;
+    profile.hourlyRate = req.body.hourlyRate || profile.hourlyRate;
+
+    const updatedProfile = await profile.save();
+
+    res.json({
+      message: "Profile Updated Successfully",
+      profile: updatedProfile,
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+const deleteProfile = async (req, res) => {
+  try {
+    const profile = await FreelancerProfile.findOne({
+      user: req.user.id
+    });
+
+    if (!profile) {
+      return res.status(404).json({
+        message: "Profile not found"
+      });
+    }
+
+    await FreelancerProfile.deleteOne({
+      user: req.user.id
+    });
+
+    res.json({
+      message: "Profile Deleted Successfully"
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      message: error.message
+    });
+  }
+};
 module.exports = {
   createProfile,
+  getProfile,
+  updateProfile,
+  deleteProfile,
 };
--- backend/routes/freelancerRoutes.js
+++ backend/routes/freelancerRoutes.js
@@ -2,9 +2,22 @@ const express = require("express");
 
 const router = express.Router();
 
-const { createProfile } = require("../controllers/freelancerController");
-const { protect } = require("../middleware/authMiddleware");
+const {
+  createProfile,
+  getProfile,
+  updateProfile,
+  deleteProfile
+} = require("../controllers/freelancerController");
 
+const {
+  protect
+} = require("../middleware/authMiddleware");
+
+// Create Profile
 router.post("/create", protect, createProfile);
 
+// Get Logged-in User Profile
+router.get("/profile", protect, getProfile);
+router.put("/profile", protect, updateProfile);
+router.delete("/profile", protect, deleteProfile);
 module.exports = router;
