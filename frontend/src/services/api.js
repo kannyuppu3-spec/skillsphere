ee1becd9f208c7cffc0d53c05193d46604479ab7
kannyuppu3-spec <kannyuppu3@gmail.com>
2026-07-19 15:05:01 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://skillsphere-qy4t.onrender.com",
+ baseURL: "https://skillsphere-qy4t.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
